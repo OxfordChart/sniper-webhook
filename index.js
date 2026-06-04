4e85bdcb159d98a7ed83e5391972f224fb121023
OxfordChart <info@oxfordchart.com>
2026-06-04 14:10:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,29 @@ const express = require('express')
 const fetch = require('node-fetch')
 const app = express()
 app.use(express.json())
+app.use(express.text())
 
 const BOT_TOKEN = process.env.BOT_TOKEN
 const CHAT_ID   = process.env.CHAT_ID
 
 app.post('/webhook', async (req, res) => {
     try {
-        const msg = req.body.text || JSON.stringify(req.body)
+        let msg = ''
+        if (typeof req.body === 'string') {
+            msg = req.body
+        } else if (req.body.text) {
+            msg = req.body.text
+        } else {
+            msg = JSON.stringify(req.body)
+        }
+
         await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ chat_id: CHAT_ID, text: msg })
+            body: JSON.stringify({ 
+                chat_id: CHAT_ID, 
+                text: msg 
+            })
         })
         res.sendStatus(200)
     } catch (err) {
